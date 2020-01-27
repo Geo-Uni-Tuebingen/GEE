b84993e78b31eff04cec13143dd0faec0954196b
AndyBraun <an.braun@uni-tuebingen.de>
2020-01-27 11:26:13 +0100

    Update S1 single image.js

--- scripts/S1 single image.js
+++ scripts/S1 single image.js
@@ -1,7 +1,7 @@
-/* Preparation
-1. Login to [EO Browser](https://apps.sentinel-hub.com/eo-browser) and search for a Sentinel-1 GRD image of your choice.  
-2. Copy the image ID into a textfile (S1A_IW_GRDH...)
-*/
+// Preparation
+// 1. Login to [EO Browser](https://apps.sentinel-hub.com/eo-browser) and search for a Sentinel-1 GRD image of your choice.  
+// 2. Copy the image ID into a textfile (S1A_IW_GRDH...)
+
   
 // Load the image
 var S1 = ee.Image('COPERNICUS/S1_GRD/S1A_IW_GRDH_1SDV_20190418T162031_20190418T162056_026846_030497_5081').select('VV');
@@ -13,4 +13,5 @@ Map.centerObject(S1);
 
 // Things to try
 // 1. Compare the image to the one displayed in the EO Browser. Are they identical?
-// 2. Have a look at the [documentation](https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_S1_GRD) how they are prepared for use in GEE
+// 2. Have a look at the documentation how they are prepared for use in GEE: 
+// https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_S1_GRD
