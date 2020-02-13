b0dfcce849425c1716ac52f91b33acd448f42d68
AndyBraun <an.braun@uni-tuebingen.de>
2020-02-13 14:32:41 +0100

    Update S1_temporal_plot.js

--- scripts/S1_temporal_plot.js
+++ scripts/S1_temporal_plot.js
@@ -12,22 +12,22 @@ var S1 = ee.ImageCollection('COPERNICUS/S1_GRD')
 .filterDate('2019-01-01', '2019-12-31')
 .filterBounds(geometry);
 
+
 // Center the map
 Map.centerObject(geometry)
 
 // Create variable for all points
-var regions1 = new ee.FeatureCollection([corn, annual_crop, pasture]); // use the class names of the polygons here
+var regions1 = new ee.FeatureCollection([corn, annual_crop, trees]);
 
 // Add one layer to the map
 Map.addLayer(S1, {min:-30, max:-5}, 'Sentinel-1 VH');
-
 
 // show the S1-HV time series chart for each vegetation type
 var S1_timeseries =
     Chart.image.seriesByRegion(S1, regions1, ee.Reducer.mean(),'VH', 30, 'system:time_start', 'label')
     .setChartType('LineChart')
     .setOptions({
-    title: 'S1 VH averages over sugarcane, annual crop and pasture', // adjust the title according to the class names
+    title: 'S1 VH averages over corn, annual crop and trees',
           vAxis: {title: 'VH [dB]',
           viewWindowMode:'explicit',
           viewWindow:{
@@ -38,7 +38,6 @@ var S1_timeseries =
           series: {
               0: {color: 'ff0000'},
               1: {color: '0000ff'},
-              2: {color: '00ff00'}}}); // color codes: https://www.color-hex.com/
-
+              2: {color: '00ff00'}}}); // https://www.color-hex.com/
 
 print(S1_timeseries);
