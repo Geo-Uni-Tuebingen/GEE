0574617ff0b1157ef3a7dc94e7ed450646db33d9
AndyBraun <an.braun@uni-tuebingen.de>
2020-02-13 14:00:48 +0100

    Update S1_single_image.js

--- scripts/S1_single_image.js
+++ scripts/S1_single_image.js
@@ -6,7 +6,7 @@
 // Load the image
 var S1 = ee.Image('COPERNICUS/S1_GRD/S1A_IW_GRDH_1SDV_20190418T162031_20190418T162056_026846_030497_5081').select('VV');
   
-Map.addLayer((S1), {min: [-15], max: [0]}, 'SAR_mean', 1);
+Map.addLayer((S1), {min: [-15], max: [0]}, 'Sentinel-1', 1);
 Map.centerObject(S1);
 
 
