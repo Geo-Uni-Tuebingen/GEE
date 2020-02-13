d42f002179f622f67b0b57eca3eeb7d6b1c0ce92
AndyBraun <an.braun@uni-tuebingen.de>
2020-02-13 19:15:32 +0100

    Update S1_temporal_average_RGB.js

--- scripts/S1_temporal_average_RGB.js
+++ scripts/S1_temporal_average_RGB.js
@@ -4,29 +4,29 @@
 // Load collection
 var sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD');
 
-// Reduce the collection
+// Reduce collection
 var s1 = sentinel1
   .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
   .filter(ee.Filter.eq('instrumentMode', 'IW'))
-  .filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'))
-  .filterDate(ee.Date('2018-01-01'), ee.Date('2018-12-31'))
+  //.select('VV')
+  .filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'))
+  .filterDate(ee.Date('2019-01-01'), ee.Date('2019-12-31'))
   .filterBounds(geometry);
 
 // Print size of filtered image collection
-print('Number of images: ', vv.size());
+print('Number of images: ', s1.size());
 
-// Temporal average (per pixel) over the entire collection
+// Temporal average (per pixel) over the entire collection // remove  filter above
 var vv_all = s1.select('VV')
 var vh_all = s1.select('VH')
 
 var vv = vv_all.mean()
 var vh = vh_all.mean()
-var cross = vv.divide(vh);
+var std = vh_all.reduce(ee.Reducer.stdDev());
 
-var RGB = ee.Image.cat(vv, vh, cross);
-Map.addLayer(RGB.clip(geometry), {min: [-15, -20, 0], max: [-5, -10, 2]}, 'composite');
+var RGB = ee.Image.cat(vv, vh, std);
+Map.addLayer(RGB.clip(geometry), {min: [-15, -20, 0], max: [-5, -10, 8]}, 'composite');
 Map.centerObject(geometry)
-
 
 // Things to try
 // 1. Instead of comparing polarizations, create an RGB of VV polarizations of three different years.
